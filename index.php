f27a0df44efcdf87248bf259391de8f06eebfa8e
Sheik1419 <sheikgamer567@gmail.com>
2025-11-23 10:43:26 +0530

    hero content

--- index.php
+++ index.php
@@ -61,8 +61,9 @@
                     <div class="hero-content">
                         <!-- Section Title Start -->
                         <div class="section-title">
-                            <h1 class="text-anime-style-2" data-cursor="-opaque"><span>Technomerates</span> Pvt Ltd</h1>
-                            <p class="wow fadeInUp" data-wow-delay="0.2s">A conglomerate of innovation, leveraging the
+                            <h1 class="text-anime-style-2 mb-2" data-cursor="-opaque">Technomerates Pvt Ltd</h1>
+                            <h3 class="wow fadeInUp fs-5 mb-2">Powered by <span>Innovation.</span> Propelled by <span>Ambition.</span></h3>
+                            <p class="wow fadeInUp mt-0 mb-3" data-wow-delay="0.2s">A conglomerate of innovation, leveraging the
                                 power of AI, IoT, and Data Science to revolutionize construction management. Our
                                 solutions redefine efficiency, safety, and decision-making, paving the way for smarter,
                                 more advanced construction processes.</p>
